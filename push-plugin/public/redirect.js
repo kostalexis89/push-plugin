9f39277a1166edbd1747baacdfb63452e58ba02a
Konstantinos Alexis <kal@ethinking.de>
2025-03-18 08:44:55 +0100

    another attempt

--- push-plugin/public/redirect.js
+++ push-plugin/public/redirect.js
@@ -1,14 +1,18 @@
-fetch("./asset-manifest.json")
-  .then((response) => response.json())
+fetch("ttps://kostalexis89.github.io/push-plugin/asset-manifest.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
   .then((manifest) => {
-    // Find the correct JS file
     const mainJsPath =
       manifest.files["main.js"] ||
       Object.values(manifest.files).find((file) => file.endsWith(".js"));
 
     if (mainJsPath) {
       const script = document.createElement("script");
-      script.src = mainJsPath.startsWith("/") ? mainJsPath : `/${mainJsPath}`;
+      script.src = `https://your-username.github.io/repo-name${mainJsPath}`;
       script.defer = true;
       document.head.appendChild(script);
     } else {
