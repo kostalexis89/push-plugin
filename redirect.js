d9ce4b8ce77677755e551cd8b60660784847899d
Konstantinos Alexis <kal@ethinking.de>
2025-03-18 08:40:48 +0100

    Updates

--- redirect.js
+++ redirect.js
@@ -1,8 +1,18 @@
 fetch("./asset-manifest.json")
   .then((response) => response.json())
   .then((manifest) => {
-    const script = document.createElement("script");
-    script.src = manifest.files["main.js"];
-    script.defer = true;
-    document.head.appendChild(script);
-  });
+    // Find the correct JS file
+    const mainJsPath =
+      manifest.files["main.js"] ||
+      Object.values(manifest.files).find((file) => file.endsWith(".js"));
+
+    if (mainJsPath) {
+      const script = document.createElement("script");
+      script.src = mainJsPath.startsWith("/") ? mainJsPath : `/${mainJsPath}`;
+      script.defer = true;
+      document.head.appendChild(script);
+    } else {
+      console.error("Main JS file not found in asset-manifest.json");
+    }
+  })
+  .catch((error) => console.error("Error loading asset-manifest.json", error));
