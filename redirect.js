76deb5ad2d17e49603ce35de529d9df5944b37ef
Konstantinos Alexis <kal@ethinking.de>
2025-04-08 13:04:18 +0200

    Updates

--- redirect.js
+++ redirect.js
@@ -7,6 +7,8 @@ fetch("https://kostalexis89.github.io/push-plugin/asset-manifest.json")
   })
   .then((manifest) => {
     const mainJsPath = manifest.files["main.js"];
+    const runtimeMainJsPath = manifest.files["runtime-main.js"];
+    const chunkJsPath = manifest.files["static/js/2.chunk.js"];
 
     if (mainJsPath) {
       const script = document.createElement("script");
@@ -15,6 +17,24 @@ fetch("https://kostalexis89.github.io/push-plugin/asset-manifest.json")
       document.head.appendChild(script);
     } else {
       console.error("Main JS file not found in asset-manifest.json");
+    }
+
+    if (runtimeMainJsPath) {
+      const script = document.createElement("script");
+      script.src = `https://kostalexis89.github.io/push-plugin${runtimeMainJsPath}`; // Fixing path
+      script.defer = true;
+      document.head.appendChild(script);
+    } else {
+      console.error("Runtime main JS file not found in asset-manifest.json");
+    }
+
+    if (chunkJsPath) {
+      const script = document.createElement("script");
+      script.src = `https://kostalexis89.github.io/push-plugin${chunkJsPath}`; // Fixing path
+      script.defer = true;
+      document.head.appendChild(script);
+    } else {
+      console.error("Chunk JS file not found in asset-manifest.json");
     }
   })
   .catch((error) => console.error("Error loading asset-manifest.json", error));
